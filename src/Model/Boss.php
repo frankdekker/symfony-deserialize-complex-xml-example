e6ae111b44ccfaa8dca1c84f3c7ef9f99fda72c7
Frank Dekker <fdekker@123inkt.nl>
2021-09-04 13:52:57 +0200

    Example to deserialize more complex object using symfony/serializer

--- src/Model/Boss.php
+++ src/Model/Boss.php
@@ -8,34 +8,38 @@ use Symfony\Component\Serializer\Annotation\SerializedName;
 
 class Boss
 {
-    private string $bossname = '';
+    /**
+     * @SerializedName("boss_name")
+     */
+    public string $bossName = '';
+
     /**
      * @SerializedName("npc")
      * @var Npc[]
      */
-    private $npcs = [];
+    private array $npcs = [];
 
+    /**
+     * Required method for the Npc[] deserialization
+     */
     public function addNpc(Npc $npc): void
     {
         $this->npcs[] = $npc;
     }
 
+    /**
+     * Required method for the Npc[] deserialization
+     */
     public function getNpcs(): array
     {
         return $this->npcs;
     }
 
+    /**
+     * Required method for the Npc[] deserialization
+     */
     public function removeNpc(Npc $npc): void
     {
-    }
-
-    public function setBossname(string $bossname): void
-    {
-        $this->bossname = $bossname;
-    }
-
-    public function getBossename(): string
-    {
-        return $this->bossname;
+        // intentionally empty, only required for deserialization
     }
 }
--- src/Model/Npc.php
+++ src/Model/Npc.php
@@ -3,10 +3,15 @@ declare(strict_types=1);
 
 namespace FD\Model;
 
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
 class Npc
 {
     public int    $id;
     public string $name;
+    /**
+     * @SerializedName("url_slug")
+     */
     public string $urlSlug;
     public int    $creatureDisplayId;
 }
